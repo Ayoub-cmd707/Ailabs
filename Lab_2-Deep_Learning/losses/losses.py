1bf089256ade0843fe420be183e0dcd16109fd23
Ayoub <s102331@ap.be>
2021-12-22 22:49:21 +0100

    out

--- Lab_2-Deep_Learning/losses/losses.py
+++ Lab_2-Deep_Learning/losses/losses.py
@@ -7,7 +7,7 @@ def mse(input_tensor: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
     """TODO: implement this method"""
     #loss = torch.nn.MSELoss()
     #output = loss(input_tensor, target)
-    temp= input_tensor-target
-    o = torch.pow(temp,2)
-    out = torch.mean(o)
-    return out
+    tensor= input_tensor-target
+    pow = torch.pow(tensor,2)
+    loss = torch.mean(pow)
+    return loss
